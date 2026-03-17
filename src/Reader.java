9fc6736b25baf4cdda007866a271206639199318
nonahachiyogo <s28385@pjwstk.edu.pl>
2026-03-17 13:09:47 +0100

    Reader class and supporting methods

---
+++ src/Reader.java
@@ -0,0 +1,23 @@
+public class Reader {
+    private String firstName;
+    private String lastName;
+    private int cardNumber;
+    private int borrowedCount;
+    public Reader(String firstName, String lastName, int cardNumber){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.cardNumber = cardNumber;
+        this.borrowedCount = 0;
+    }
+    public void printData(){
+        System.out.println(firstName + " " + lastName +
+                ", Card number: " +  cardNumber +
+                ", borrowed: "+ borrowedCount);
+    }
+    public void increaseBorrowedCount(){
+        borrowedCount++;
+    }
+    public void decreaseBorrowedCount(){
+        if (borrowedCount > 0 ) borrowedCount--;
+    }
+}
